823f187ad8f0a4ce91729e68af68356d3193702a
Jon Smith <smithj@shedit.net.au>
2018-08-24 09:30:01 +1000

    Initial Project Files

---
+++ src/webparts/fullBleedWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFullBleedWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FullBleedWebPartWebPartStrings' {
+  const strings: IFullBleedWebPartWebPartStrings;
+  export = strings;
+}
